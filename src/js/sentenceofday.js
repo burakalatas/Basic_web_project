2413f8ec2f8f0efb115f76b378fe3957a9f9474c
Burak Alatas <burak.alatas2011@gmail.com>
2022-07-13 13:51:27 +0300

    sentenceOfDay and clock added!

---
+++ src/js/sentenceofday.js
@@ -0,0 +1,38 @@
+
+quotes = new Array();
+quotes[7, 13] = "Başarısız olursan hayal kırıklığına uğrayabilirsin ama denemezsen mahvolursun.";
+quotes[7, 14] = "Yapmadığınız atışların %100'ünü kaçırırsınız."
+quotes[7, 15] = "Başarı, başarısızlıktan başarısızlığa, coşku kaybı olmadan yürümektir."
+quotes[7, 16] = "Başarılı bir insan, başkalarının ona attığı tuğlalarla sağlam bir temel oluşturabilen kişidir."
+quotes[7, 17] = "Mutlu olmayı yarına bırakmak, karşıya geçmek için nehrin durmasını beklemeye benzer ve bilirsin o nehir asla durmaz."
+quotes[7, 18] = "Hayattaki en büyük zafer benim için hiçbir zaman düşmemekte değil, her düştüğünde ayağa kalkmakta yatar."
+quotes[7, 19] = "Hayatta en kıymetli hediye zamandır. Kime hediye ettiğinize dikkat edin."
+quotes[7, 20] = "Her ağlayan güçsüz değildir. Tıpkı her gülenin mutlu olmadığı gibi."
+quotes[7, 21] = "Asla başka insanlar üzülmesin diye kendini üzme. Sen kaldırabiliyorsan onlar da kaldırabilir. - Bob Marley"
+quotes[7, 22] = ""
+quotes[7, 23] = ""
+
+
+var a = new Date();
+var dayOfMonth = a.getDate();
+var MonthOfYear = a.getMonth() + 1;
+
+//display the quotation
+if (quotes[MonthOfYear, dayOfMonth] != "" && quotes[MonthOfYear, dayOfMonth] != undefined) {
+    document.getElementById("sentenceofday").innerHTML = "\"" + quotes[MonthOfYear, dayOfMonth] + "\"";
+}
+if (dayOfMonth < 10) {
+    var c = dayOfMonth;
+    dayOfMonth = "0";
+    dayOfMonth += c;
+
+}
+if (MonthOfYear < 10) {
+    var c = MonthOfYear;
+    MonthOfYear = "0";
+    MonthOfYear += c;
+}
+var year = a.getFullYear();
+
+document.getElementById("date").innerHTML = dayOfMonth + "." + MonthOfYear + "." + year;
+
---
+++ src/js/timer.js
@@ -0,0 +1,44 @@
+
+// Set the date we're counting down to
+var countDownDate = new Date("oct 16, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "HAPPY BIRTHDAY TO ME";
+    }
+}, 1000);
+
+window.onload = setInterval(clock, 1000);
+function clock() {
+    // Get today's date and time
+    var nowHours = new Date().getHours();
+    var nowMinutes = new Date().getMinutes();
+    if (nowMinutes < 10) {
+        var c = nowMinutes;
+        nowMinutes = "0";
+        nowMinutes += c;
+    }
+
+    document.getElementById("time").innerHTML = nowHours + ":" + nowMinutes;
+}
+
